ce8097c8d0e9fd25c7365e521ed39f93096e8b07
Marcelo <marcelococcaro@email.com>
2024-03-05 15:50:02 -0300

    Testando códigos do test

---
+++ test.py
@@ -0,0 +1,8 @@
+#determina o comprimento de uma cadeia de caracteres
+print(len("Técnicas"))
+#concatena 2 cadeias  de caracteres
+print("Ténicas "+" de Programação")
+#converte uma cadeia de caracteres em letras maiúsculas
+print("Técnicas".upper())
+#extrai uma subcadeia de caracteres
+print("Técnicas"[0:5])
